3db4cdd98ddbf32862c27e1067f3d15ce833c23b
matiasgueli <matiasgueli@gmail.com>
2017-04-05 17:06:58 -0300

    Inicio

---
+++ RinconArtesano/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RinconArtesano.Startup))]
+namespace RinconArtesano
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
